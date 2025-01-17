4b6dfb7abd346769b47b34f8e5a1af3e4cf62d62
iamjonn <jonata10.jn@gmail.com>
2025-01-17 02:05:24 -0300

    apresentacao de mensagen de error

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("botaologin").addEventListener("click", function () {
       const email = document.getElementById("email").value;
       const password = document.getElementById("senha").value;
+      
+      const emailError = document.getElementById("emailErrado");
+      const passwordError = document.getElementById("senhaErrada");
+      const mensagem = document.getElementById("mensagem");
+
+
+      let isValid = true;
 
       // limpar
       emailError.style.display = "none";
       passwordError.style.display = "none";
+      mensagem.textContent = "";
 
       // Validar email
       if (!email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
@@ -14,9 +22,15 @@ document.addEventListener("DOMContentLoaded", function () {
         isValid = false;
     }
       
+      //exebir mensagens de sucesso ou error
+      if (isValid) {
+        mensagem.textContent = "Login realizado com sucesso!";
+        mensagem.style.color = "green";
+      } else {
+        mensagem.textContent = "Corrija os erros acima.";
+        mensagem.style.color = "red";
+      }
 
-      
-
-      
+        
  });
 });
